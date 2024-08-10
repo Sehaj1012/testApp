1a71ffe09285012898ecf85774350c2696a68e84
Sehaj1012 <fujiyamasenju@gmail.com>
2024-08-09 23:17:25 -0600

    Front-end
    Added a login page and a registration page
    
    Also added links on main page

---
+++ app/login/page.jsx
@@ -0,0 +1,31 @@
+"use client"
+import { useState } from 'react'
+import React from 'react'
+
+const Login = () => {
+    const [ UserName, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const onChangeUserName = (e) => {
+        setUserName(e.target.value);
+    }
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitting", password, UserName);
+    }
+
+  return (
+        <form onSubmit={onSubmit} className='flex gap-6 flex-col items-start p-3'>
+            <input type="username" className='border-black border-2 rounded p-3 w-1/6'  placeholder='username' onChange={onChangeUserName}/>
+            <input type="password" className='border-black border-2 rounded p-3  w-1/6'placeholder='password' onChange={onChangePassword} />
+            <button type='submit' className='bg-gray-500 p-2 rounded'>Login</button>
+        </form>
+  )
+}
+
+export default Login
